ddec39d53b0b5534ff4c20f35f13b9ba8ed92027
windo-developer <yui050505@gmail.com>
2021-03-24 15:07:00 +0900

    AWS

--- src/post/uploadPost/uploadPost.typeDefs.ts
+++ src/post/uploadPost/uploadPost.typeDefs.ts
@@ -2,6 +2,6 @@ import { gql } from "apollo-server-core";
 
 export default gql`
   type Mutation {
-    uploadPost(title: String!, contents: String, file: String): Post
+    uploadPost(title: String!, contents: String, file: Upload): Post
   }
 `;
